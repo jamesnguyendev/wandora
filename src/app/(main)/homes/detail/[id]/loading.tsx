28cfe5303e65a9be72fb87f8a016385b897420d6
jamesnguyendev <jamesnguyen.devs@gmail.com>
2025-11-10 22:43:58 +0700

    fix: update skeleton for detail

--- src/app/(main)/homes/detail/[id]/loading.tsx
+++ src/app/(main)/homes/detail/[id]/loading.tsx
@@ -1,5 +1,32 @@
+import { Card } from "@/components/ui/card";
+import { Skeleton } from "@/components/ui/skeleton";
+
 const loading = () => {
-  return <div>loading...</div>;
+  return (
+    <div className="flex justify-center">
+      <div className="my-10 w-full rounded-lg border p-10 md:w-1/2 lg:w-2/4">
+        <div className="flex gap-3">
+          <Skeleton className="h-16 w-24" />
+          <div className="flex flex-col gap-2">
+            <Skeleton className="h-3 w-20" />
+            <Skeleton className="h-3 w-10" />
+            <Skeleton className="h-3 w-10" />
+          </div>
+        </div>
+
+        <div className="my-5 flex flex-col gap-3 xl:flex-row">
+          <Skeleton className="h-80 w-full" />
+          <Skeleton className="h-80 w-full" />
+        </div>
+
+        <Skeleton className="my-3 h-2 w-full" />
+        <Skeleton className="h-4 w-20" />
+        <Skeleton className="my-3 h-2 w-full" />
+
+        <Skeleton className="h-10 w-full" />
+      </div>
+    </div>
+  );
 };
 
 export default loading;
--- src/components/common/error-ui.tsx
+++ src/components/common/error-ui.tsx
@@ -2,8 +2,6 @@
 
 import { useEffect } from "react";
 
-import Link from "next/link";
-
 import * as Sentry from "@sentry/nextjs";
 
 import { Button } from "@/components/ui/button";
@@ -19,8 +17,8 @@ export default function ErrorUI({ error, reset }: { error: Error; reset: () => v
         <div className="flex min-h-screen flex-col items-center justify-center space-y-4 text-center">
           <div>
             <h1 className="text-5xl font-bold text-red-500">500</h1>
-            <h2 className="mb-2 text-xl font-semibold">Internal Error</h2>
-            <p>Something went wrong.</p>
+            <h2 className="mb-2 text-xl font-semibold">Networking Error</h2>
+            <p>Bad networking.</p>
             <p>{error.message}</p>
           </div>
 
