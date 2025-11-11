63ce0cfbdd90973804f8c19517f5af87ff38e146
jamesnguyendev <jamesnguyen.devs@gmail.com>
2025-11-11 20:27:20 +0700

    chore: update logo app in auth page

--- src/app/(main)/auth/layout.tsx
+++ src/app/(main)/auth/layout.tsx
@@ -1,6 +1,6 @@
 import { ReactNode } from "react";
 
-import { Command } from "lucide-react";
+import Link from "next/link";
 
 import { Separator } from "@/components/ui/separator";
 import { APP_CONFIG } from "@/config/app-config";
@@ -11,8 +11,10 @@ export default function Layout({ children }: Readonly<{ children: ReactNode }>) 
       <div className="grid h-dvh justify-center p-2 lg:grid-cols-2">
         <div className="bg-primary relative order-2 hidden h-full rounded-3xl lg:flex">
           <div className="text-primary-foreground absolute top-10 space-y-1 px-10">
-            <Command className="size-10" />
-            <h1 className="text-2xl font-medium">{APP_CONFIG.name}</h1>
+            <Link href={"/"} className="font-serif text-4xl font-bold italic">
+              W.
+            </Link>
+            <h1 className="mt-1 text-2xl font-medium">{APP_CONFIG.name}</h1>
             <p className="text-sm">Design. Build. Launch. Repeat.</p>
           </div>
 
